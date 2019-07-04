33eb552f9d163e7543267347196f0919dfe08d2a
sohyoun <mongswell69@naver.com>
2019-07-05 04:12:45 +0900

    0705

--- hotpicks/src/main/java/com/kitri/hotpicks/contents/model/ReviewDto.java
+++ hotpicks/src/main/java/com/kitri/hotpicks/contents/model/ReviewDto.java
@@ -1,5 +1,85 @@
 package com.kitri.hotpicks.contents.model;
 
 public class ReviewDto {
-
+	private int seq;
+	private String userId;
+	private int contentsId;
+	private String hashTag;
+	private String subject;
+	private int starPoint;
+	private String content;
+	private String logTime;
+	private String saveFolder;
+	private String orignPicture;
+	private String savePicture;
+	
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getContentsId() {
+		return contentsId;
+	}
+	public void setContentsId(int contentsId) {
+		this.contentsId = contentsId;
+	}
+	public String getHashTag() {
+		return hashTag;
+	}
+	public void setHashTag(String hashTag) {
+		this.hashTag = hashTag;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getStarPoint() {
+		return starPoint;
+	}
+	public void setStarPoint(int starPoint) {
+		this.starPoint = starPoint;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getLogTime() {
+		return logTime;
+	}
+	public void setLogTime(String logTime) {
+		this.logTime = logTime;
+	}
+	public String getSaveFolder() {
+		return saveFolder;
+	}
+	public void setSaveFolder(String saveFolder) {
+		this.saveFolder = saveFolder;
+	}
+	public String getOrignPicture() {
+		return orignPicture;
+	}
+	public void setOrignPicture(String orignPicture) {
+		this.orignPicture = orignPicture;
+	}
+	public String getSavePicture() {
+		return savePicture;
+	}
+	public void setSavePicture(String savePicture) {
+		this.savePicture = savePicture;
+	}
+	
+	
 }
